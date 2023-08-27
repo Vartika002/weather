07b7620544e1933a0ce84bcf78fd173de79e8421
Vartika <vartika@resolveindia.com>
2023-08-28 00:01:56 +0530

    Temp change with change of city name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [data, setdata] = useState({});
+  const [data, setData] = useState({});
+  const [inputCity, setInputCity] = useState("");
 
   const apiKey = "6b0e492bae1d4405d7dd9606723f248c";
-  const apiURL =
-    "https://pro.openweathermap.org/data/2.5/forecast/climate?q=London&appid=6b0e492bae1d4405d7dd9606723f248c";
-  const getWeatherDetails = () => {
-    axios.get();
+
+  const getWeatherDetails = (cityName) => {
+    if (!cityName) return;
+    const apiURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=" +
+      apiKey;
+    axios
+      .get(apiURL)
+      .then((response) => {
+        console.log("res", response?.data);
+        setData(response?.data);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
   };
+
+  const handleChanegInputCity = (e) => {
+    setInputCity(e.target.value);
+  };
+
+  const handleSearch = () => {
+    getWeatherDetails(inputCity);
+  };
+
+  useEffect(() => {
+    getWeatherDetails("delhi");
+  }, []);
+
   return (
     <div className="col-md-12">
       <div className="weatherBg">
         <h1 className="heading">Weather App</h1>
         <div className="d-grid gap-3 col-4 mt-3">
-          <input type="text" className="form-control" />
-          <button className="btn btn-primary" type="button">
+          <input
+            type="text"
+            className="form-control"
+            onChange={handleChanegInputCity}
+            value={inputCity}
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={handleSearch}
+          >
             Search
           </button>
         </div>
@@ -30,8 +66,10 @@ function App() {
             src="https://i.pinimg.com/564x/77/0b/80/770b805d5c99c7931366c2e84e88f251.jpg"
           />
 
-          <h5 className="weatherCity">Delhi</h5>
-          <h6 className="weatherTemp">18°C</h6>
+          <h5 className="weatherCity">{data?.name}</h5>
+          <h6 className="weatherTemp">
+            {(data?.main?.temp - 273.15).toFixed(2)}°C
+          </h6>
         </div>
       </div>
     </div>
